27d2779a414c14c07bb1e2198ebb8cce16b9f384
OlezhaStudenyak <100136889+OlezhaStudenyak@users.noreply.github.com>
2023-06-29 23:44:14 +0300

    node_basics-lab

---
+++ 4Node_basics/src/runSequent.ts
@@ -0,0 +1,19 @@
+const runSequent = (arr: Array<unknown>, callback: Function): Array<unknown> => {
+  return arr.map((item, index) => callback(item, index));
+}
+
+const arr: Array<string> = ["one", "two", "three"];
+
+const processArray = async () => {
+  const results = runSequent(arr, (item: string, index: number) => Promise.resolve({
+    item,
+    index,
+  })
+  );
+
+  return Promise.all(results);
+}
+
+processArray().then(data => {
+  console.log(data);
+});
